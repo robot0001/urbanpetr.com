1a44409e3b6cb5051eb538d84c658bccf1ed6b3a
Petr Urban <urban.peta@gmail.com>
2025-11-19 14:44:53 +0100

    Fix Terraform formatting

--- terraform/s3_test.tf
+++ terraform/s3_test.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tmp_test" {
-  bucket = "urbanpetr-tmp-test-01"  # dej něco globálně unikátního
+  bucket = "urbanpetr-tmp-test-01" # dej něco globálně unikátního
 
   tags = {
     Project = var.project_name
