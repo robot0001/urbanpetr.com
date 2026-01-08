0d574538063a36c8d28aa8d547b00b114d5058b6
Petr Urban <robot0001@Petrs-MacBook-Pro-M4Pro.local>
2026-01-08 12:57:55 +0100

    Update key for terraform

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "urbanpetr-tf-state"
-    key            = "state/terraform.tfstate"
+    key            = "urbanpetr_website/prod/terraform.tfstate" 
     dynamodb_table = "terraform-locks"
     region         = "eu-central-1"
     encrypt        = true
