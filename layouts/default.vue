0a2a4d1a78f1ecbe891ec43bd891dde0386177fa
Petr Urban <urban.peta@gmail.com>
2026-04-25 16:21:57 +0200

    Fix nav (#29)

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,3 +8,12 @@ nav(class="sticky top-0 z-50 border-b backdrop-blur-sm bg-white/80 dark:bg-black
 main(class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12")
   slot
 </template>
+
+<style scoped>
+a {
+  color: inherit;
+}
+a.router-link-exact-active {
+  color: theme('colors.orange.500');
+}
+</style>
